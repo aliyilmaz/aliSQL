0801a5d58267b28d64db2434fabedbb7cd25d72b
Ali YIlmaz <aliyilmaz.work@gmail.com>
2018-12-07 12:57:45 +0300

    The "update" test was created.

---
+++ tests/update.php
@@ -0,0 +1,45 @@
+<?php
+require_once '../src/Mind.php';
+
+use Mind\Mind;
+
+$Mind = new Mind();
+
+/**
+ * Record update.
+ *
+ * @param string   $tblname
+ * @param mixed   $arr
+ * @param mixed   $id
+ * @return  bool
+ * */
+$rows = array(
+    'name'          => 'Ali Yılmaz'.rand(1,100),
+);
+
+$tblname = 'phonebook';
+if(!$Mind->update($tblname, $rows, 1)){
+    exit('Error: Record updated');
+}
+echo '<h5>Record updated. </h5>';
+
+/**
+ * Record update.
+ *
+ * @param string   $tblname
+ * @param mixed   $arr
+ * @param mixed   $id
+ * @param mixed   $special
+ * @return  bool
+ * */
+$rows = array(
+    'name'          => 'Ali Yılmaz'.rand(1,100),
+);
+
+$tblname = 'phonebook';
+if(!$Mind->update($tblname, $rows, 'Ali Yılmaz95', 'name')){
+    exit('Error: Record updated. (special)');
+}
+echo '<h5>Record updated. (special)</h5>';
+
+echo '<h1>The record update method is running.</h1>';
