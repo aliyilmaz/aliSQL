af1b3a84c31b2cae6c4f8e3040f869157a64f0d2
Ali YIlmaz <aliyilmaz.work@gmail.com>
2018-12-28 13:04:02 +0300

    The is_json test was created.

---
+++ tests/is_json.php
@@ -0,0 +1,16 @@
+<?php
+require_once '../src/Mind.php';
+
+use Mind\Mind;
+
+$Mind = new Mind();
+
+$schema = array(
+    'test'=>'ali'
+);
+
+if($Mind->is_json(json_encode($schema))){
+    echo 'This is a json syntax.';
+} else {
+    echo 'This is not a json syntax.';
+}
