aeaef531f6d9524f1ad13177d4cc6ae9bca0e82f
aliyilmaz <aliyilmaz.work@gmail.com>
2019-05-17 14:41:13 +0300

    Metod adı güncellendi, $data değişkeni silindi.

--- src/Mind.php
+++ src/Mind.php
@@ -1673,14 +1673,14 @@ class Mind {
      * @param $url
      * @return mixed
      */
-    public function rfilesize($url){
+    public function remote_filesize($url){
         $ch = curl_init($url);
 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
         curl_setopt($ch, CURLOPT_HEADER, TRUE);
         curl_setopt($ch, CURLOPT_NOBODY, TRUE);
 
-        $data = curl_exec($ch);
+        curl_exec($ch);
         $size = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
 
         curl_close($ch);
