4b6187d3138fdfbe093372c9335634e38889c367
aliyilmaz <aliyilmaz.work@gmail.com>
2019-05-17 23:21:52 +0300

    Version 2.3.7

--- src/Mind.php
+++ src/Mind.php
@@ -3,7 +3,7 @@
 /**
  *
  * @package    Mind
- * @version    Release: 2.3.6
+ * @version    Release: 2.3.7
  * @license    GPLv3
  * @author     Ali YILMAZ <aliyilmaz.work@gmail.com>
  * @category   Php Framework, Design pattern builder for PHP.
