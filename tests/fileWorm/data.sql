0ce26077f5a679e05469f22e65c1f195d96e5bec
aliyilmaz <aliyilmaz.work@gmail.com>
2019-06-22 01:54:37 +0300

    fileWorm için çalışma ortamı hazırlandı.

---
+++ tests/fileWorm/data.sql
@@ -0,0 +1,100 @@
+Jackson Rowe	WAT29WNY8ER	arcu.Sed.et@eudui.net	Bermuda	Dunfermline
+Noah Pratt	ATQ00ZWR4BX	vehicula.risus.Nulla@lobortismauris.edu	Heard Island and Mcdonald Islands	San Martino in Badia/St. Martin in Thurn
+Moses Wall	APE25HYC3SX	eu@nonenim.com	Namibia	Villa Faraldi
+Barclay Cooke	IXF70UCL9BJ	Nulla.facilisi.Sed@Nulla.com	Ukraine	Caplan
+Orlando Cunningham	HQO09SOG8ZT	laoreet@nonarcu.org	Curaçao	Woerden
+Caldwell Riley	KSZ76NIM5GQ	Integer@nasceturridiculus.edu	Portugal	Durham
+Chaim Justice	ZMC90ROG3AH	Donec@neque.org	Ecuador	Ahmadpur East
+Carlos Alford	ZRL86PRZ7FG	nisi.Cum.sociis@bibendum.org	Georgia	Maipú
+Bert Hess	FGC98FXQ3FJ	turpis.Nulla.aliquet@loremeumetus.co.uk	Turkey	Penicuik
+Bruce Simmons	GWA94WFD7ON	montes.nascetur.ridiculus@eratvitaerisus.co.uk	Hungary	Radicofani
+Yasir Castillo	EPW91AQH9FY	ante.iaculis.nec@orciquis.edu	Brunei	Okotoks
+Christian Key	NHO27YTY7EM	nec.malesuada@mollisduiin.com	Pitcairn Islands	Linton
+Berk Guy	ZPI76XKK6FM	Lorem.ipsum@Sedidrisus.net	Bahamas	Scorrano
+Kennedy Figueroa	XIL83BHP0ZL	pharetra@Quisquevarius.net	Botswana	Sautin
+Hop Jefferson	AVD32APX8NP	massa.lobortis.ultrices@dolor.co.uk	Cayman Islands	Houston
+Berk Cobb	RDQ25YFV4DS	consectetuer@risus.ca	Libya	Sars-la-Buissire
+Devin Villarreal	BAB35PLI5TU	sapien.molestie.orci@sagittissemperNam.ca	Saudi Arabia	Newtown
+Brenden Molina	PFT79AOT2VK	aliquam.enim.nec@magna.edu	Iran	St. Clears
+Curran Chen	DJS35WRN7KT	nec.tellus@vulputateveliteu.edu	Gabon	Saint Paul
+Dale Wagner	ETQ48HQC0GC	elementum.dui@massa.ca	French Guiana	Graven Grez-Doiceau
+Hammett Matthews	KGD66HJW1WC	tristique.senectus@egestas.org	Kyrgyzstan	Dongelberg
+Oscar Hendricks	DHE74MUC6DZ	tincidunt@elitpretium.org	Bahamas	Pergola
+Xanthus Gibbs	IRT15SHI2XM	metus.In.lorem@estac.co.uk	Kiribati	Santa Cruz
+Clarke Hines	DVT34NWS0GF	Aliquam@montes.org	Dominica	Poggiodomo
+Jakeem Rodriquez	UTU38DIB0QL	Duis@Phasellus.org	Switzerland	Civitacampomarano
+Jordan Hyde	WAN43SBZ7AP	diam.Pellentesque.habitant@duiaugueeu.edu	Bouvet Island	Saltcoats
+Hashim Davenport	LGT21IZC9BF	non@erosturpis.org	Marshall Islands	Meridian
+Victor Hayes	NQV40YNN0WL	nulla.Cras.eu@aliquetmetusurna.edu	Puerto Rico	Taber
+Timothy Mcdowell	RPA24GLX8YN	luctus.ipsum@urnaVivamusmolestie.com	Mozambique	Langley
+Quinn Schwartz	WOX49AJZ0CN	malesuada.id@felispurus.ca	Maldives	Chiniot
+Edward Colon	RNX55LDH7YX	gravida.Praesent@ridiculus.edu	Solomon Islands	Cumberland
+Blaze Frost	YIC57KVR2EX	elementum.sem@nislelementumpurus.co.uk	Italy	Sint-Martens-Lennik
+Bradley Pollard	FAL94END8HS	felis@nasceturridiculus.ca	South Georgia and The South Sandwich Islands	Montignies-Saint-Christophe
+Hunter Chan	DYG63SFQ0EV	Duis.gravida.Praesent@adipiscingMaurismolestie.com	Belgium	Pontypridd
+Maxwell Valencia	EQJ07SPM7SO	sem@lobortisauguescelerisque.edu	Armenia	Ranchi
+Boris Brown	XHY98PVG1FK	est@Quisque.net	Albania	Salzwedel
+Blaze Shepard	PSU56JKN5BY	ac@blanditviverra.net	Saint Helena, Ascension and Tristan da Cunha	Marbella
+Neville Knapp	EZW60GSE1XV	dis.parturient@velarcu.net	Panama	Kieldrecht
+Alden Chaney	PHM48ZKK9SL	eu.eros.Nam@sapienimperdiet.com	Ireland	Guardia Perticara
+Elijah Reid	BPD38MWD3FI	velit.Quisque@nibhQuisque.com	Libya	Villers-la-Bonne-Eau
+Boris Mcfadden	DML72CHO3ZZ	Nulla.facilisis@Aliquam.ca	Afghanistan	Patos
+Malcolm Nieves	HLO44POJ2AV	Proin.velit@cursusdiamat.net	Tanzania	Besançon
+Brandon Knapp	LQM00PCP8TW	Etiam.imperdiet@turpisnon.ca	Tanzania	Juseret
+Nash Malone	RBS86GFF0CC	mus.Donec.dignissim@erat.co.uk	Azerbaijan	Beauvechain
+Gareth Justice	HGP06ZJX9UY	semper.erat@atiaculisquis.ca	Cameroon	Owensboro
+Scott Bond	YGW92OLR9SL	augue.eu@nuncestmollis.edu	San Marino	Grandrieu
+Noah Phelps	XBI56KOP8LC	Donec.porttitor.tellus@Fusce.co.uk	Malta	Grobbendonk
+Erich Peck	ATG18THA1EJ	id.libero@dolorNulla.co.uk	Saint Barthélemy	New Westminster
+Jared Mccormick	RXB55HVL5MF	quis.pede.Praesent@senectuset.edu	Uruguay	Jerzu
+Laith Nicholson	MYW28NEM5AS	nec.mauris.blandit@magnaPhasellus.com	Indonesia	Uttarpara-Kotrung
+Kamal Kerr	HYC78DHM8KS	nibh.sit.amet@nuncnullavulputate.ca	Barbados	Gespeg
+Alden Spears	FOP38XHD2KO	mattis.semper@fringillaporttitor.co.uk	India	Montelupo Fiorentino
+Emerson Quinn	TKX78POD2QL	orci@cursusIntegermollis.ca	Central African Republic	Archennes
+Colby Gilbert	OJV57DJZ0GJ	sagittis.felis@feugiat.ca	Saudi Arabia	Salzwedel
+Hyatt Avila	HND37VUX2YL	Mauris.magna.Duis@pede.net	Marshall Islands	Brye
+Caldwell Dawson	XMP19OWA5NJ	per.conubia.nostra@ettristique.org	Mexico	Fuenlabrada
+Marvin Ballard	NXZ12TEL7UA	tincidunt@dolorDonecfringilla.co.uk	Estonia	Shimla
+Armando Velasquez	BCL82MXV0UZ	Quisque.imperdiet.erat@in.edu	Macedonia	Zuienkerke
+Byron Barr	KFV59ARR2LB	ac.turpis@pretiumet.co.uk	Singapore	Neerheylissem
+Nissim Montoya	SXO84SAD1SF	sit.amet@ultriciesadipiscing.net	Guyana	Milnathort
+Quinlan Austin	OKH79ESC1DE	Nunc.mauris@Aliquam.org	Libya	Dollard-des-Ormeaux
+Zeus Olson	KCC81UOP6VS	vulputate@aodiosemper.net	Zambia	Cap-de-la-Madeleine
+Lester Booker	GMR12ICW8NZ	Proin.nisl@duiSuspendisse.net	Ecuador	Santo Stefano del Sole
+Victor Harvey	PYC37IYA1QR	purus.sapien.gravida@at.com	Uruguay	Beho
+Tanek Price	CQY52JYL0KI	fermentum.convallis@etmalesuadafames.co.uk	Belgium	Paredones
+Driscoll Bullock	DXW13WBO3WH	ac@gravidamolestiearcu.org	Mali	Itter
+Ryan Fowler	HCJ00XBH7OS	est.tempor@ridiculusmus.edu	Belarus	Bearberry
+Dane Franklin	PEQ71SZU6IK	et.ultrices.posuere@ac.ca	Croatia	Kitscoty
+Barclay Alvarado	CZI59BTO8ID	ac.mattis.ornare@egetipsumSuspendisse.co.uk	American Samoa	Buckingham
+Tate Herman	UQL27BBR3HM	malesuada.fames@dictumcursusNunc.net	Niger	Ragogna
+Uriah Porter	HPH44DVN6ZR	eu@tristiquesenectuset.net	Madagascar	Speyer
+Erich Burch	LVS74SMW7ZG	ante.dictum@Proinnislsem.org	Morocco	Ciudad Real
+Quinn Strong	TOM25ZVS7FO	lacus.Etiam.bibendum@arcu.ca	Burundi	Bournemouth
+Uriel Mcmillan	HXR24NYG7RH	augue@loremvitae.co.uk	South Georgia and The South Sandwich Islands	Candela
+Felix Bartlett	RHS00GIG4BL	quis.lectus@condimentumegetvolutpat.ca	Côte D'Ivoire (Ivory Coast)	Khanewal
+Felix Dillon	FHC96SWW2MP	lobortis.risus@dolorQuisque.edu	Guinea	Bünyan
+Hamilton Dodson	UXF30ANJ1XB	lorem.lorem.luctus@dolorsit.ca	Viet Nam	Vorst
+Caesar Byers	SHY60NVK5DN	augue.eu.tempor@tellusNunclectus.ca	Spain	Kimberly
+Armand Garcia	XOU73ILA1FG	nunc@nequepellentesquemassa.com	Saint Lucia	Port Coquitlam
+Colton Donovan	UAG64YFF9QP	elit.fermentum@sitamet.co.uk	Saint Helena, Ascension and Tristan da Cunha	Campochiaro
+Vance Wolfe	RBH03SNY4IQ	erat.vitae.risus@Sed.ca	Malawi	Cauquenes
+Steven Ware	MPJ68XIP8RG	fermentum.metus.Aenean@Morbivehicula.net	Serbia	Governador Valadares
+Jerome Atkins	RNS82NJI0TU	purus.Duis.elementum@diamProindolor.edu	Bolivia	Petrolina
+Vernon Sharpe	JOL36ZLN4OG	feugiat.Lorem@seddolor.edu	Sri Lanka	Windermere
+Adam Michael	XGQ91CTQ7BF	semper.et@arcuSedet.ca	Rwanda	Owen Sound
+Uriah Holmes	DAU78XWS9BR	orci.Ut@nisiaodio.co.uk	China	Arras
+Holmes Ward	IUD18LCF2FI	lorem.Donec.elementum@inconsequat.edu	Togo	Offenburg
+Amir Patel	DZN27HAY1XK	pretium.neque@Mauris.edu	Saint Kitts and Nevis	Jonqui�re
+Connor Moss	GNN45PPS2QL	id.ante@etultricesposuere.ca	Kenya	Priero
+Julian Puckett	YKV11YIV9WS	magna.malesuada.vel@ullamcorpervelit.ca	Suriname	Indianapolis
+Carter Phelps	TMB69WWF2SO	nascetur@acipsumPhasellus.ca	Curaçao	Rotem
+Hamish Calderon	QLN69LHE0DA	lorem.fringilla.ornare@dolor.org	Solomon Islands	Parndorf
+Colt Alexander	RXA71BMB2MH	pharetra.nibh.Aliquam@velnislQuisque.net	Jersey	Macklin
+Jeremy Quinn	EDX14GMA2VE	Nulla.dignissim@mi.net	Cyprus	Candidoni
+Jakeem Scott	NKO44FFU7VA	posuere@felisadipiscingfringilla.org	Bulgaria	Macon
+Eagan Chen	XBZ92HEP7KT	amet@accumsanconvallisante.net	Bolivia	Bon Accord
+Merrill Gates	JXD84EDZ6GK	magnis@eueleifend.net	El Salvador	North Dum Dum
+Joel Cameron	IEQ40ZNH9SI	velit@liberolacus.com	Montenegro	Eernegem
+Reece Stein	WRJ29UCB8HK	lorem.ut@Mauris.org	Uzbekistan	SŽlange
+Tobias Ward	CFK17KSE4AK	sed@Duismi.ca	Saint Kitts and Nevis	Ville de Maniwaki
--- tests/fileWorm/fileWorm.php
+++ tests/fileWorm/fileWorm.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../src/Mind.php';
+require_once('../../src/Mind.php');
 
 $Mind = new Mind();
 
