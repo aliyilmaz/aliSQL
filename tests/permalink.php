cc194517c7961ad0b939192c1710fd39a5125f9c
aliyilmaz <aliyilmaz.work@gmail.com>
2019-07-11 22:15:02 +0300

    Permalink kullanım örneklerine test edildi.

--- tests/permalink.php
+++ tests/permalink.php
@@ -4,4 +4,53 @@ require_once ('../src/Mind.php');
 $Mind = new Mind();
 
 
-echo $Mind->permalink('Çok çalışırsan başarırsın.', array('lowercase'=>false));
+//$str = 'Merhaba dünya';
+//echo $Mind->permalink($str);
+
+//$str = 'Merhaba dünya';
+//$option = array(
+//    'delimiter' => '_'
+//);
+//echo $Mind->permalink($str, $option);
+
+//$str = 'Merhaba dünya';
+//$option = array(
+//    'limit'=>'3'
+//);
+//echo $Mind->permalink($str, $option);
+
+//$str = 'Merhaba dünya';
+//$option = array(
+//    'limit'=>3
+//);
+//echo $Mind->permalink($str, $option);
+
+//$str = 'Merhaba dünya';
+//$option = array(
+//    'lowercase'=>false
+//);
+//echo $Mind->permalink($str, $option);
+
+//$str = 'Merhaba dünya';
+//$option = array(
+//    'replacements'=>array(
+//        'Merhaba' => 'hello',
+//        'dünya' => 'world'
+//    )
+//);
+//echo $Mind->permalink($str, $option);
+
+//$str = 'Merhaba dünya';
+//$option = array(
+//    'transliterate'=>false
+//);
+//echo $Mind->permalink($str, $option);
+
+$str = 'Merhaba dünya';
+$option = array(
+    'replacements'=>array(
+        'Merhaba' => 'hello'
+    ),
+    'transliterate'=>false
+);
+echo $Mind->permalink($str, $option);
