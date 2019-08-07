56430d8aacbd9cb0de89e5f0a2768659411959c8
aliyilmaz <aliyilmaz.work@gmail.com>
2019-08-07 23:28:21 +0300

    Foreign key özelliği eklendi.

--- tests/delete.php
+++ tests/delete.php
@@ -12,7 +12,21 @@ if($Mind->delete('users', 2)){
 
 echo '<br>';
 if($Mind->delete('users', array(1,4))){
-    echo 'Kayıtlar silindi';
+    echo 'Kayıtlar silindi.';
+} else {
+    echo 'Kayıtlar silinemedi.';
+}
+
+echo '<br>';
+if($this->delete('users', 1, array('posts'=>'author_id', 'gallery'=>'author_id'))){
+    echo 'Kayıtlar silindi.';
+} else {
+    echo 'Kayıtlar silinemedi.';
+}
+
+echo '<br>';
+if($this->delete('users', array(1,3), array('posts'=>'author_id', 'gallery'=>'author_id'))){
+    echo 'Kayıtlar silindi.';
 } else {
     echo 'Kayıtlar silinemedi.';
 }
