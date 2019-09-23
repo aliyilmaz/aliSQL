39478ab3f181541d7d982487a0f1492d4acb0589
aliyilmaz <aliyilmaz.work@gmail.com>
2019-09-23 18:09:20 +0300

    Versiyon 3.0.7 sürümüne yükseltildi.

--- src/Mind.php
+++ src/Mind.php
@@ -3,7 +3,7 @@
 /**
  *
  * @package    Mind
- * @version    Release: 3.0.6
+ * @version    Release: 3.0.7
  * @license    GPLv3
  * @author     Ali YILMAZ <aliyilmaz.work@gmail.com>
  * @category   Php Framework, Design pattern builder for PHP.
