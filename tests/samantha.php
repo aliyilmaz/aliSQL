45de77473c1369135b46ab6ebb483ed4cd2989e7
aliyilmaz <aliyilmaz.work@gmail.com>
2019-10-11 22:04:04 +0300

    Samantha güncellendi.

--- tests/samantha.php
+++ tests/samantha.php
@@ -2,6 +2,64 @@
 require_once '../src/Mind.php';
 
 $Mind = new Mind();
+//
+//
+//$scheme = array(
+//    'id:increments',
+//    'username:string',
+//    'password:string',
+//    'email:string',
+//    'avatar:small',
+//    '_token:small',
+//    'created_at:string',
+//    'updated_at:string'
+//);
+//$Mind->tableCreate('users', $scheme);
+//
+//$scheme = array(
+//    array(
+//        'username'=>'Tilo Mitra',
+//        'password'=>md5('123456'),
+//        'email'=>'tilo.mitra@example.com',
+//        'avatar'=>'public/img/common/tilo-avatar.png',
+//        '_token'=>md5(rand(999,9999)),
+//        'created_at'=>$Mind->timestamp
+//    ),
+//    array(
+//        'username'=>'Eric Ferraiuolo',
+//        'password'=>md5('123456'),
+//        'email'=>'eric.ferraiuolo@example.com',
+//        'avatar'=>'public/img/common/ericf-avatar.png',
+//        '_token'=>md5(rand(999,9999)),
+//        'created_at'=>$Mind->timestamp
+//    ),
+//    array(
+//        'username'=>'Reid Burke',
+//        'password'=>md5('123456'),
+//        'email'=>'reid.burke@example.com',
+//        'avatar'=>'public/img/common/reid-avatar.png',
+//        '_token'=>md5(rand(999,9999)),
+//        'created_at'=>$Mind->timestamp
+//    ),
+//    array(
+//        'username'=>'Andrew Wooldridge',
+//        'password'=>md5('123456'),
+//        'email'=>'andrew.wooldridge@example.com',
+//        'avatar'=>'public/img/common/andrew-avatar.png',
+//        '_token'=>md5(rand(999,9999)),
+//        'created_at'=>$Mind->timestamp
+//    )
+//);
+//$scheme = array(
+//    'username'=>'Deniz',
+//    'password'=>md5('123456'),
+//    'email'=>'deniz@example.com',
+//    'avatar'=>'public/img/common/deniz.png',
+//    '_token'=>md5(rand(999,9999)),
+//    'created_at'=>$Mind->timestamp
+//);
+
+//$Mind->insert('users', $scheme);
 
 echo '<br>';
 
@@ -41,3 +99,44 @@ echo '<br>';
     public/img/common/tilo-avatar.png
 */
 echo $Mind->samantha('users', array('id'=>'1'), 'avatar' );
+
+echo '<br>';
+
+/*
+ * Array
+(
+    [0] => Array
+        (
+            [username] => Tilo Mitra
+            [password] => e10adc3949ba59abbe56e057f20f883e
+        )
+
+    [1] => Array
+        (
+            [username] => Eric Ferraiuolo
+            [password] => e10adc3949ba59abbe56e057f20f883e
+        )
+
+    [2] => Array
+        (
+            [username] => Reid Burke
+            [password] => e10adc3949ba59abbe56e057f20f883e
+        )
+
+    [3] => Array
+        (
+            [username] => Andrew Wooldridge
+            [password] => e10adc3949ba59abbe56e057f20f883e
+        )
+
+    [4] => Array
+        (
+            [username] => Deniz
+            [password] => e10adc3949ba59abbe56e057f20f883e
+        )
+
+)
+ */
+echo '<pre>';
+print_r($Mind->samantha('users', array('password'=>'e10adc3949ba59abbe56e057f20f883e'), array('username', 'password')));
+echo '</pre>';
