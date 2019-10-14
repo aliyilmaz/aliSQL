966176a7476d660c50b9b13e27e6e06182fd8721
aliyilmaz <aliyilmaz.work@gmail.com>
2019-10-14 16:39:55 +0300

    is_url metodu güncellendi ve güncelleştirme test'e yansıtıldı.

--- tests/is_url.php
+++ tests/is_url.php
@@ -4,46 +4,74 @@ require_once '../src/Mind.php';
 $Mind = new Mind();
 
 
+echo '<hr>';
 
 $str = 'http://localhost';
 if($Mind->is_url($str)){
-    echo 'Bu bir bağlantıdır.';
+    echo 'Bu bir bağlantıdır. <strong>'.$str.'</strong>';
 } else {
     echo 'Bu bir bağlantı değildir.';
+}
+
+echo '<hr>';
+
+$str = 'https://127.0.0.1/test';
+if($Mind->is_url($str)){
+    echo 'Bu bir bağlantıdır. <strong>'.$str.'</strong>';
+} else {
+    echo 'Bu bir bağlantı değildir.';
+}
+
+echo '<hr>';
+
+$str = 'https://test.example.com/test';
+if($Mind->is_url($str)){
+    echo 'Bu bir bağlantıdır. <strong>'.$str.'</strong>';
+} else {
+    echo 'Bu bir bağlantı değildir. <strong>'.$str.'</strong>';
+}
+
+echo '<hr>';
+
+$str = 'https://test-example.com/test';
+if($Mind->is_url($str)){
+    echo 'Bu bir bağlantıdır. <strong>'.$str.'</strong>';
+} else {
+    echo 'Bu bir bağlantı değildir. <strong>'.$str.'</strong>';
 }
 
 echo '<hr>';
 
 $str = 'example.com';
 if($Mind->is_url($str)){
-    echo 'Bu bir bağlantıdır.';
+    echo 'Bu bir bağlantıdır. <strong>'.$str.'</strong>';
 } else {
-    echo 'Bu bir bağlantı değildir.';
+    echo 'Bu bir bağlantı değildir. <strong>'.$str.'</strong>';
 }
 
 echo '<hr>';
 
 $str = 'http://example.com';
 if($Mind->is_url($str)){
-    echo 'Bu bir bağlantıdır.';
+    echo 'Bu bir bağlantıdır. <strong>'.$str.'</strong>';
 } else {
-    echo 'Bu bir bağlantı değildir.';
+    echo 'Bu bir bağlantı değildir. <strong>'.$str.'</strong>';
 }
 
 echo '<hr>';
 
 $str = 'http://www.example.com';
 if($Mind->is_url($str)){
-    echo 'Bu bir bağlantıdır.';
+    echo 'Bu bir bağlantıdır. <strong>'.$str.'</strong>';
 } else {
-    echo 'Bu bir bağlantı değildir.';
+    echo 'Bu bir bağlantı değildir. <strong>'.$str.'</strong>';
 }
 
 echo '<hr>';
 
 $str = 'https://www.example.com';
 if($Mind->is_url($str)){
-    echo 'Bu bir bağlantıdır.';
+    echo 'Bu bir bağlantıdır. <strong>'.$str.'</strong>';
 } else {
-    echo 'Bu bir bağlantı değildir.';
+    echo 'Bu bir bağlantı değildir. <strong>'.$str.'</strong>';
 }
