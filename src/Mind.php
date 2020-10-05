478d94316962875cf6e9bdb919a096ec2a237f00
Ali Yılmaz <aliyilmaz.work@gmail.com>
2020-10-05 16:57:45 +0300

    Release: 3.1.8

--- src/Mind.php
+++ src/Mind.php
@@ -3,7 +3,7 @@
 /**
  *
  * @package    Mind
- * @version    Release: 3.1.8
+ * @version    Release: 3.2.0
  * @license    GPL3
  * @author     Ali YILMAZ <aliyilmaz.work@gmail.com>
  * @category   Php Framework, Design pattern builder for PHP.
