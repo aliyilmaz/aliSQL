d2d371350952437323cbdf10514553423e4a07f4
Ali Yılmaz <aliyilmaz.work@gmail.com>
2020-11-06 01:45:54 +0300

    Comment update.

--- tests/getTableDetail.php
+++ tests/getTableDetail.php
@@ -6,6 +6,17 @@ $Mind = new Mind();
 
 $Mind->selectDB('test1');
 
+// Array
+// (
+//     [0] => id:increments:11
+//     [1] => username:string:255
+//     [2] => password:string:255
+//     [3] => description:medium
+//     [4] => address:large
+//     [5] => amount:decimal:10,0
+//     [6] => age:int:11
+// )
+
 echo '<pre>';
 print_r($Mind->getTableDetail('users'));
 echo '</pre>';
