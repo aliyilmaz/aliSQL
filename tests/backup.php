55da451bb41f5870fb4ef800482a938908ba28ff
Ali Yılmaz <aliyilmaz.work@gmail.com>
2020-11-06 17:57:15 +0300

    Backup and Restore test file updated

--- tests/backup.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once '../src/Mind.php';
-
-$Mind = new Mind();
-
-// $Mind->backup('mydb');
-
-$Mind->backup(array('mydb', 'trek'));
--- tests/restore/restore.php
+++ tests/restore/restore.php
@@ -4,7 +4,5 @@ require_once '../../src/Mind.php';
 
 $Mind = new Mind();
 
-// $Mind->restore('mydb');
-echo '<pre>';
-print_r($Mind->restore(array('../../tests/restore/backup.json')));
-echo '</pre>';
+$Mind->restore('backup_2020_11_06_17_40_21.json');
+// $Mind->restore(array('backup_2020_11_06_17_40_21.json', 'backup_2020_11_06_17_41_22.json'));
