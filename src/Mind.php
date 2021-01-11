36a1da5c6db6cff739221f64c5f7a3b78db38b0f
aliyilmaz <aliyilmaz.work@gmail.com>
2021-01-11 19:52:54 +0300

    Mind has been upgraded to version 4.1.5.

--- src/Mind.php
+++ src/Mind.php
@@ -3,7 +3,7 @@
 /**
  *
  * @package    Mind
- * @version    Release: 4.1.4
+ * @version    Release: 4.1.5
  * @license    GPL3
  * @author     Ali YILMAZ <aliyilmaz.work@gmail.com>
  * @category   Php Framework, Design pattern builder for PHP.
